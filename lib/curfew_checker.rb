019bf81f8d556ec5e66e208ea415712adf67e006
Grace5275 <grace_madson@outlook.com>
2018-07-25 13:56:19 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -3,20 +3,22 @@ def simple_curfew_checker(time)
     puts "It's past curfew!"
   end
 end
-
+ # code goes here
+ 
 simple_curfew_checker(14)
-# def curfew_checker(time)
-#   # code goes here
-# end
 
-# def complex_curfew_checker(time)
-#   # code goes here
-# end
+def curfew_checker(time)
+  # code goes here
+end
 
-# def deluxe_curfew_checker(time)
-#   # code goes here
-# end
+def complex_curfew_checker(time)
+  # code goes here
+end
 
-# def platinum_curfew_checker(current_time, curfew_time)
-#   # code goes here
-# end
+def deluxe_curfew_checker(time)
+  # code goes here
+end
+
+def platinum_curfew_checker(current_time, curfew_time)
+  # code goes here
+end
