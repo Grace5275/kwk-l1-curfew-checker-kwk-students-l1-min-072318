3b69c2f491dfcfcf92dd285ea6f50376c8604840
Grace5275 <grace_madson@outlook.com>
2018-07-25 14:12:58 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,36 +1,44 @@
-def simple_curfew_checker(time)
-  if time >= 11
-    puts "you're in trouble"
-  end
-end
- # code goes here
+# def simple_curfew_checker(time)
+#   if time >= 11
+#     puts "you're in trouble"
+#   end
+# end
+# # code goes here
  
 
-def curfew_checker(time)
-  if time >= 11
-    puts "you're in trouble"
-  else 
-    puts "you can keep having fun with the time you have left"
-  end
-  # code goes here
-end
+# def curfew_checker(time)
+#   if time >= 11
+#     puts "you're in trouble"
+#   else 
+#     puts "you can keep having fun with the time you have left"
+#   end
+#   # code goes here
+# end
 
-def complex_curfew_checker(time)
-  if time == 11
-    puts "you better head home"
-  elsif time > 11
-    puts "you're in trouble"
-  else 
-    puts "you can keep having fun in the time you have left"
-  end
-  # code goes here
-end
+# def complex_curfew_checker(time)
+#   if time == 11
+#     puts "you better head home"
+#   elsif time > 11
+#     puts "you're in trouble"
+#   else 
+#     puts "you can keep having fun in the time you have left"
+#   end
+#   # code goes here
+# end
 
 def deluxe_curfew_checker(time)
-  
+  curfew = 11
+  curfew - time = time_left 
+   if time == curfew 
+    puts "you better head home"
+  elsif time > curfew
+    puts "you're in trouble"
+  else 
+    puts "you can keep having fun in the #{timeleft} hours you have left"
+  end
   # code goes here
 end
 
-def platinum_curfew_checker(current_time, curfew_time)
-  # code goes here
-end
+# def platinum_curfew_checker(current_time, curfew_time)
+#   # code goes here
+# end
